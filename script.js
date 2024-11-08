75a060a0fe4f84f84bc2034bcd348159662d9abc
Cumayevomr <cumayevomr8@gmail.com>
2024-11-08 17:05:54 +0400

    line group end

--- script.js
+++ script.js
@@ -2,14 +2,40 @@ const navDialog = document.getElementById('nav-dialog');
 function handleMenu() {
     navDialog.classList.toggle('hidden');
 }
+
+const initialTranslateLTR = -48*4;
+const initialTranslateRTL = 36*4;
+
+
+
+
 function setupIntersectionObserver(element, isLTR, speed) {
     const intersectionCsllback = (entries) => {
         const isIntersecting = entries[0].isIntersecting;
-        console.log(element, isIntersecting);
-        
+        if(isIntersecting) {
+            document.addEventListener('scroll', scrollHandler);
+        } else{
+            document.removeEventListener('scroll', scrollHandler);
+        }
     }
     const intersectionObserver = new IntersectionObserver(intersectionCsllback);
     intersectionObserver.observe(element);
+    function scrollHandler() {
+
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if(isLTR) {
+            totalTranslate = translateX + initialTranslateLTR;
+        } else {
+            totalTranslate = -(translateX + initialTranslateRTL);
+        }
+
+        element.style.transform = `translateX(${totalTranslate}px)`;
+
+
+    }
+
 }
 
 const line1 = document.getElementById('line1');
