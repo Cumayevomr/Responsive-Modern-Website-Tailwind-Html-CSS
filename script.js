b3739861319d769e78383d05b2b5682996849601
Cumayevomr <cumayevomr8@gmail.com>
2024-11-08 16:04:41 +0400

    center line group

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const navDialog = document.getElementById('nav-dialog');
 function handleMenu() {
     navDialog.classList.toggle('hidden');
 }
+function setupIntersectionObserver(element, isLTR, speed) {
+    const intersectionCsllback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        console.log(element, isIntersecting);
+        
+    }
+    const intersectionObserver = new IntersectionObserver(intersectionCsllback);
+    intersectionObserver.observe(element);
+}
+
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
